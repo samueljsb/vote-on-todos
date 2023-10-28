0b777c00b82006b9beb6af0b6f998240c00f5f9d
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2023-10-28 01:34:06 +0100

    Redirect root to lists view

--- vote_on_todos/website/urls.py
+++ vote_on_todos/website/urls.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='lists/')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('lists/', views.Lists.as_view(), name='lists'),
